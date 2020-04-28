f72eada66ee2b171ae2c14e776bc53521ae80145
@analyticd <4829djaskdfj@gmail.com>
2020-04-28 16:23:00 -0700

    Comment out invocation of setting bg color and fuzzy enhancements
    These don't work well with my config.

--- helm-ido-like.el
+++ helm-ido-like.el
@@ -1,4 +1,3 @@
-
 (defun helm-ido-like-activate-helm-modes ()
   (require 'helm-config)
   (helm-mode 1)
@@ -68,15 +67,15 @@ Its element is a pair of `buffer-name' and `mode-line-format'.")
   (add-hook 'helm-cleanup-hook #'helm-ido-like-bottom-buffers-show-mode-line)
   (advice-add 'helm-keyboard-quit :around #'helm-ido-like-helm-keyboard-quit-advice))
 
-(defun helm-ido-like-hide-helm-modeline-1 ()
-  "Hide mode line in `helm-buffer'."
-  (with-helm-buffer
-    (setq-local mode-line-format nil)))
+  (defun helm-ido-like-hide-helm-modeline-1 ()
+    "Hide mode line in `helm-buffer'."
+    (with-helm-buffer
+      (setq-local mode-line-format nil)))
 
 
-(defun helm-ido-like-hide-helm-modeline ()
-  (fset 'helm-display-mode-line #'ignore)
-  (add-hook 'helm-after-initialize-hook 'helm-ido-like-hide-helm-modeline-1))
+  (defun helm-ido-like-hide-helm-modeline ()
+    (fset 'helm-display-mode-line #'ignore)
+    (add-hook 'helm-after-initialize-hook 'helm-ido-like-hide-helm-modeline-1))
 
 (defvar helm-ido-like-source-header-default-background nil)
 (defvar helm-ido-like-source-header-default-foreground nil)
@@ -114,41 +113,41 @@ Its element is a pair of `buffer-name' and `mode-line-format'.")
 (defun helm-ido-like-setup-bg-color ()
   (add-hook 'helm-after-initialize-hook 'helm-ido-like-setup-bg-color-1))
 
-(defun helm-ido-like-find-files-up-one-level-maybe ()
-  (interactive)
-  (if (looking-back "/" 1)
-      (call-interactively 'helm-find-files-up-one-level)
-    (delete-char -1)))
+  (defun helm-ido-like-find-files-up-one-level-maybe ()
+    (interactive)
+    (if (looking-back "/" 1)
+        (call-interactively 'helm-find-files-up-one-level)
+      (delete-char -1)))
 
 
-(defun helm-ido-like-find-files-navigate-forward (orig-fun &rest args)
-  "Adjust how helm-execute-persistent actions behaves, depending on context."
-  (let ((sel (helm-get-selection)))
-    (if (file-directory-p sel)
-        ;; the current dir needs to work to
-        ;; be able to select directories if needed
-        (cond ((and (stringp sel)
-                    (string-match "\\.\\'" (helm-get-selection)))
-               (helm-maybe-exit-minibuffer))
-              (t
-               (apply orig-fun args)))
-      (helm-maybe-exit-minibuffer))))
+  (defun helm-ido-like-find-files-navigate-forward (orig-fun &rest args)
+    "Adjust how helm-execute-persistent actions behaves, depending on context."
+    (let ((sel (helm-get-selection)))
+      (if (file-directory-p sel)
+          ;; the current dir needs to work to
+          ;; be able to select directories if needed
+          (cond ((and (stringp sel)
+                      (string-match "\\.\\'" (helm-get-selection)))
+                 (helm-maybe-exit-minibuffer))
+                (t
+                 (apply orig-fun args)))
+        (helm-maybe-exit-minibuffer))))
 
 
-(defun helm-ido-like-load-file-nav ()
-  (advice-add 'helm-execute-persistent-action :around #'helm-ido-like-find-files-navigate-forward)
-    ;; <return> is not bound in helm-map by default
-  (define-key helm-map (kbd "<return>") 'helm-maybe-exit-minibuffer)
-  (with-eval-after-load 'helm-files
-    (define-key helm-read-file-map (kbd "<backspace>") 'helm-ido-like-find-files-up-one-level-maybe)
-    (define-key helm-read-file-map (kbd "DEL") 'helm-ido-like-find-files-up-one-level-maybe)
-    (define-key helm-find-files-map (kbd "<backspace>") 'helm-ido-like-find-files-up-one-level-maybe)
-    (define-key helm-find-files-map (kbd "DEL") 'helm-ido-like-find-files-up-one-level-maybe)
+  (defun helm-ido-like-load-file-nav ()
+    (advice-add 'helm-execute-persistent-action :around #'helm-ido-like-find-files-navigate-forward)
+      ;; <return> is not bound in helm-map by default
+    (define-key helm-map (kbd "<return>") 'helm-maybe-exit-minibuffer)
+    (with-eval-after-load 'helm-files
+      (define-key helm-read-file-map (kbd "<backspace>") 'helm-ido-like-find-files-up-one-level-maybe)
+      (define-key helm-read-file-map (kbd "DEL") 'helm-ido-like-find-files-up-one-level-maybe)
+      (define-key helm-find-files-map (kbd "<backspace>") 'helm-ido-like-find-files-up-one-level-maybe)
+      (define-key helm-find-files-map (kbd "DEL") 'helm-ido-like-find-files-up-one-level-maybe)
 
-    (define-key helm-find-files-map (kbd "<return>") 'helm-execute-persistent-action)
-    (define-key helm-read-file-map (kbd "<return>") 'helm-execute-persistent-action)
-    (define-key helm-find-files-map (kbd "RET") 'helm-execute-persistent-action)
-    (define-key helm-read-file-map (kbd "RET") 'helm-execute-persistent-action)))
+      (define-key helm-find-files-map (kbd "<return>") 'helm-execute-persistent-action)
+      (define-key helm-read-file-map (kbd "<return>") 'helm-execute-persistent-action)
+      (define-key helm-find-files-map (kbd "RET") 'helm-execute-persistent-action)
+      (define-key helm-read-file-map (kbd "RET") 'helm-execute-persistent-action)))
 
 (defvar helm-ido-like-no-dots-whitelist
   '("*Helm file completions*")
@@ -218,20 +217,20 @@ Its element is a pair of `buffer-name' and `mode-line-format'.")
   (add-hook 'helm-cleanup-hook #'helm-ido-like-fuzzier-activate)
   (advice-add 'helm-keyboard-quit :before #'helm-ido-like-fuzzier-activate))
 
-;;;###autoload
-(defun helm-ido-like ()
-  "Configure and activate `helm', `helm-fuzzier' and `helm-flx'."
-  (interactive)
-  (helm-ido-like-activate-helm-modes)
-  (helm-ido-like-load-ido-like-bottom-buffer)
-  (helm-ido-like-hide-modelines)
-  (helm-ido-like-hide-helm-modeline)
-  (helm-ido-like-header-lines-maybe)
-  (helm-ido-like-setup-bg-color)
-  (helm-ido-like-load-file-nav)
-  (helm-ido-like-no-dots)
-  (helm-ido-like-load-fuzzy-enhancements)
-  (helm-ido-like-fix-fuzzy-files))
+  ;;;###autoload
+  (defun helm-ido-like ()
+    "Configure and activate `helm', `helm-fuzzier' and `helm-flx'."
+    (interactive)
+    (helm-ido-like-activate-helm-modes)
+    (helm-ido-like-load-ido-like-bottom-buffer)
+    (helm-ido-like-hide-modelines)
+    (helm-ido-like-hide-helm-modeline)
+    (helm-ido-like-header-lines-maybe)
+    ;; (helm-ido-like-setup-bg-color)
+    (helm-ido-like-load-file-nav)
+    (helm-ido-like-no-dots)
+    ;; (helm-ido-like-load-fuzzy-enhancements)
+    (helm-ido-like-fix-fuzzy-files))
 
-(provide 'helm-ido-like)
-;;; helm-ido-like.el ends here
+  (provide 'helm-ido-like)
+  ;;; helm-ido-like.el ends here
